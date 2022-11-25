1f3e2adb106970f4fd47d763414e60a4909fd945
Kelvin Kamau <kelvinkam24@gmail.com>
2022-11-25 08:01:29 +0300

    Added all the files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useState} from 'react';
+import { data } from './data';
 
-function App() {
+const App = () => {
+ const [index,setIndex] = useState(0);
+ const {job,image,name} = data[index];
+
+ const checkNumber = (number)=>{
+  if(number > data.length-1){
+     return 0;
+  }
+  if(number < 0){
+   return data.length-1
+  }
+  return number;
+ }
+
+ const nextItem = ()=>{
+  setIndex((index)=>{
+   let newIndex = index + 1;
+   return checkNumber(newIndex);
+  })
+ }
+  const prevItem = ()=>{
+   setIndex((index)=>{
+    let newIndex = index - 1;
+    return checkNumber(newIndex);
+   })
+ }
+ const randomImage = ()=>{
+  let randomNumber = Math.floor(Math.random()*data.length);
+
+  if(randomNumber === index){
+   return randomNumber = index + 1;
+  }
+  setIndex(checkNumber(randomNumber))
+ };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section>
+     <img src={image} alt={name} className='image'/>
+     <h4>{name}</h4>
+     <p>{job}</p>
+     <div className='btn-holder'>
+      <button type='button' onClick={nextItem}>Next</button>
+      <button type='button' onClick={prevItem}>Previous</button>
+     </div>
+     <button onClick={randomImage}>Random</button>
+    </section>
+  )
 }
 
-export default App;
+export default App
